98e523d69cb1174d93cfecde6b54c6cf6b5c0f2e
Antoine-Juret <antoine.juret@edu.esiee.fr>
2025-01-15 12:22:30 +0000

    docstring command

--- command.py
+++ command.py
@@ -1,32 +1,34 @@
-# This file contains the Command class.
+"""
+Ce module contient la classe `Command`, qui représente une commande. 
+Chaque commande est définie par un mot-clé, un texte d'aide, une action à exécuter, 
+et le nombre de paramètres requis.
+
+Classes:
+    Command: Représente une commande et fournit les informations 
+    nécessaires pour son exécution.
+
+Usage:
+    Ce module est utilisé dans le cadre d'un moteur de jeu 
+    où des commandes textuelles sont interprétées pour exécuter des actions.
+
+"""
 
 class Command:
     """
-    This class represents a command. A command is composed of a command word, a help string, an action and a number of parameters.
+    Représente une commande dans le jeu. Une commande est composée d'un mot-clé, d'un texte d'aide, 
+    d'une action à exécuter et du nombre de paramètres requis.
 
     Attributes:
-        command_word (str): The command word.
-        help_string (str): The help string.
-        action (function): The action to execute when the command is called.
-        number_of_parameters (int): The number of parameters expected by the command.
+        command_word (str): Le mot-clé de la commande.
+        help_string (str): La description ou l'aide associée à la commande.
+        action (fonction): La fonction à exécuter lorsque la commande est appelée.
+        number_of_parameters (int): Le nombre de paramètres attendus par la commande.
 
     Methods:
-        __init__(self, command_word, help_string, action, number_of_parameters) : The constructor.
-        __str__(self) : The string representation of the command.
-
-    Examples:
-
-    >>> from actions import go
-    >>> command = Command("go", "Permet de se déplacer dans une direction.", go, 1)
-    >>> command.command_word
-    'go'
-    >>> command.help_string
-    'Permet de se déplacer dans une direction.'
-    >>> type(command.action)
-    <class 'function'>
-    >>> command.number_of_parameters
-    1
-
+        __init__(self, command_word, help_string, action, number_of_parameters):
+            Initialise une nouvelle instance de la commande.
+        __str__(self):
+            Retourne une représentation textuelle de la commande.
     """
 
     # The constructor.
@@ -35,11 +37,8 @@ class Command:
         self.help_string = help_string
         self.action = action
         self.number_of_parameters = number_of_parameters
-    
+
     # The string representation of the command.
     def __str__(self):
         return  self.command_word \
                 + self.help_string
-    
-
-
